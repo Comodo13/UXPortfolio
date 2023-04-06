a3458af4202602d34a95cc411617d5fe3082354c
Alkash113 <alipchenkoartem@gmail.com>
2023-04-06 18:06:08 +0200

    f

--- components/Footers/FooterOne.tsx
+++ components/Footers/FooterOne.tsx
@@ -14,7 +14,7 @@ export default function Footer(props: Props) {
       <div className="mx-auto max-w-7xl overflow-hidden py-4 px-8 sm:py-8 lg:px-8">
       
   <p className="mt-10 text-center text-md leading-5 text-gray-500">
-          &copy; {text} . Wszystkie prawa zastrzeżone.
+          &copy; {text}. Wszystkie prawa zastrzeżone.
         </p>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
         *DISCLAIMER: Ten produkt nie jest przeznaczony do zastąpienia profesjonalnej porady medycznej, diagnozy lub leczenia. Wszelkie pytania dotyczące stanu zdrowia należy zawsze konsultować z lekarzem lub innym wykwalifikowanym pracownikiem medycznym.
