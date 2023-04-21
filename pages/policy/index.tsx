fa67411b0ddc5e8ab2ede2541f309debc166e567
Dima <dzmitry.rabotkin@woltair.cz>
2023-04-21 17:16:25 +0200

    vv

--- pages/policy/index.tsx
+++ pages/policy/index.tsx
@@ -7,49 +7,49 @@ export default function Policy() {
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Política de privacidad</h1>
         <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Protección de datos personales</h2>
         <p className="mt-6 text-xl leading-8">
-          Hemos introducido instalaciones de seguridad para proteger sus datos personales cuando ingresa, transmite o
-          trabaja con su información personal.
+        Wprowadziliśmy zabezpieczenia w celu ochrony Twoich danych osobowych podczas wprowadzania, przesyłania lub pracy z Twoimi danymi osobowymi.
+        pracy z Twoimi danymi osobowymi.
         </p>
         <div className="mt-10 max-w-2xl">
           <h2 className="mt-8 text-2xl font-bold tracking-tight text-gray-900">
-            Divulgación de información privada y su transmisión a terceros
+          Ujawnianie informacji prywatnych i ich przekazywanie osobom trzecim
           </h2>
           <p className="mt-8">
-            Su información privada puede ser revelada por nosotros solo si es necesaria para: (a) garantizar el
-            cumplimiento de la disposición de la ley o los requisitos de los procedimientos judiciales relativos a
-            nuestro sistema; (b) protección de nuestros derechos o propiedad; (c) tomar medidas inmediatas con respecto
-            a la seguridad personal de nuestro personal o consumidores de servicios, así como con respecto a la
-            seguridad pública. Su información privada que tenemos a nuestra disposición cuando se registre, puede ser
-            transmitida a terceros y personas que cooperan con nosotros con el fin de mejorar la calidad de nuestros
-            servicios. Sus datos privados no se utilizarán para ningún otro propósito que no sea el mencionado
-            anteriormente. La dirección de correo electrónico que nos proporciona cuando se registra puede utilizarse
-            para enviarle mensajes o notificaciones sobre cambios relacionados con su solicitud.
+          Państwa prywatne informacje mogą zostać ujawnione przez nas tylko wtedy, gdy jest to konieczne do: (a) zapewnienia zgodności z przepisem prawa lub wymogami postępowania prawnego
+            (a) zapewnienia zgodności z przepisem prawa lub wymogami postępowania prawnego dotyczącego naszego systemu; (b) ochrony naszych praw lub własności
+            naszego systemu; (b) ochrony naszych praw lub własności; (c) podjęcia natychmiastowych działań w odniesieniu do bezpieczeństwa osobistego naszych pracowników lub konsumentów naszych usług oraz
+            bezpieczeństwa osobistego naszego personelu lub konsumentów usług, jak również w odniesieniu do bezpieczeństwa publicznego.
+            bezpieczeństwo publiczne. Twoje prywatne informacje, które udostępniamy podczas rejestracji, mogą być
+            przekazywane osobom trzecim oraz osobom współpracującym z nami w celu poprawy jakości naszych usług.
+            usług. Twoje prywatne dane nie będą wykorzystywane do innych celów niż wymienione powyżej.
+            powyżej. Adres e-mail podany przez Ciebie podczas rejestracji może być wykorzystany do wysyłania Ci wiadomości lub powiadomień o naszych usługach.
+            do wysyłania Ci wiadomości lub powiadomień o zmianach związanych z Twoją aplikacją.
           </p>
           <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">El uso de "cookies"</h2>
           <p className="mt-8">
-            Cuando un usuario visita un sitio web, el archivo "cookie" se registra en su computadora (si un usuario
-            permite la recepción de dichos archivos). Si un usuario ya ha visitado este sitio web, la cookie del archivo
-            se lee desde la computadora. Además de otros fines, utilizamos cookies para facilitar la grabación de
-            estadísticas de visitantes. Dichos datos nos ayudan a determinar qué tipo de información enviada a los
-            clientes puede proporcionarles más valor. Esta recopilación de datos se lleva a cabo de forma general y
-            nunca afecta a la información privada del usuario.
+          Kiedy użytkownik odwiedza stronę internetową, na komputerze użytkownika zapisywany jest plik "cookie" (jeśli użytkownik zezwala na takie pliki).
+            zezwala na odbiór takich plików). Jeśli użytkownik już odwiedził tę stronę, plik "cookie" jest odczytywany z komputera użytkownika.
+            plik jest odczytywany z komputera. Oprócz innych celów, używamy plików cookies, aby ułatwić rejestrowanie
+            statystyk odwiedzin. Takie dane pomagają nam określić, jakie informacje wysyłane do klientów mogą stanowić dla nich największą wartość.
+            klientom dostarczyć największą wartość. To zbieranie danych odbywa się na zasadach ogólnych i nie
+            nigdy nie wpływa na prywatne informacje użytkownika.
           </p>
           <p className="mt-6">
-            Los terceros, incluido Google, muestran los anuncios publicitarios de nuestra empresa en las páginas web de
-            Internet. Los terceros, incluido Google, utilizan cookies para mostrar anuncios basados en las visitas
-            previas de los usuarios a nuestros sitios web y sus intereses en los navegadores web. El usuario puede
-            prohibirle a Google que use cookies. Para hacerlo, debe ir a la página especial de Google en esta dirección:
+          Podmioty trzecie, w tym Google, wyświetlają reklamy naszej firmy na stronach w Internecie.
+            Internecie. Strony trzecie, w tym Google, wykorzystują pliki cookie do wyświetlania reklam w oparciu o poprzednie wizyty użytkowników na naszych stronach internetowych oraz ich zainteresowania w ich przeglądarkach internetowych.
+            poprzednich wizyt użytkowników na naszych stronach internetowych oraz ich zainteresowań w ich przeglądarkach internetowych. Użytkownik może
+            zabronić Google korzystania z plików cookie. W tym celu należy przejść na specjalną stronę Google pod adresem:
             http://www.google.com/privacy/ads/
           </p>
           <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-            Cambios en la aplicación de divulgación confidencial
+          Zmiany we wniosku o ujawnienie informacji poufnych
           </h2>
           <p className="mt-10">
-            La aplicación de divulgación confidencial debe actualizarse periódicamente. A partir de entonces, se
-            modificará la fecha de la actualización anterior indicada al comienzo del documento. Las notificaciones
-            sobre estos cambios se colocarán en un lugar destacado de nuestras páginas web.
+          Wniosek o ujawnienie informacji poufnych musi być okresowo aktualizowany. Po tym czasie data poprzedniej
+            data poprzedniej aktualizacji wskazana na początku dokumentu zostanie zmieniona. Powiadomienia o niniejszych zmianach
+            Powiadomienia o tych zmianach będą umieszczane w widocznym miejscu na naszych stronach internetowych.
           </p>
-          <p className="mt-6 text-center font-bold">¡Gracias por mostrar interés en nuestro sistema!</p>
+          <p className="mt-6 text-center font-bold">Dziękujemy za okazanie zainteresowania naszym systemem!</p>
         </div>
       </div>
     </div>
