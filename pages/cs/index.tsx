7c131cfe2d536381880fc597523e573ded9fff12
Dima <dzmitry.rabotkin@woltair.cz>
2023-11-03 14:44:05 +0100

    d

--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -10,38 +10,7 @@ import {
   import fink from '../../public/logos/fink.jpg'
 import { PaperClipIcon } from '@heroicons/react/24/solid'
 
-  const features = [
-    {
-      name: 'Push to deploy.',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-      icon: CloudArrowUpIcon,
-    },
-    {
-      name: 'SSL certificates.',
-      description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-      icon: LockClosedIcon,
-    },
-    {
-      name: 'Simple queues.',
-      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.',
-      icon: ArrowPathIcon,
-    },
-    {
-      name: 'Advanced security.',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-      icon: FingerPrintIcon,
-    },
-    {
-      name: 'Powerful API.',
-      description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-      icon: Cog6ToothIcon,
-    },
-    {
-      name: 'Database backups.',
-      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ',
-      icon: ServerIcon,
-    },
-  ]
+
   
   export default function Example() {
     return (
