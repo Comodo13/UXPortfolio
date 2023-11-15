91a01ccd54cd29d3d9a4924c2a54ab075de1c04a
Dima <dzmitry.rabotkin@woltair.cz>
2023-11-15 16:34:00 +0100

    dd

---
+++ pages/toyland/index.tsx
@@ -0,0 +1,56 @@
+import {
+    ArrowPathIcon,
+    CloudArrowUpIcon,
+    Cog6ToothIcon,
+    FingerPrintIcon,
+    LockClosedIcon,
+    PlayIcon,
+    AcademicCapIcon,
+    BanknotesIcon,
+    ServerIcon,
+    RocketLaunchIcon,
+    CheckCircleIcon,
+    BoltIcon,
+    HandThumbUpIcon,
+    HandThumbDownIcon,
+    UserGroupIcon,
+  
+  } from '@heroicons/react/20/solid'
+  
+  import {
+    FaceSmileIcon,
+    FaceFrownIcon,
+  } from '@heroicons/react/24/outline'
+  
+  
+  import Image from 'next/image'
+  import fink from '../../public/logos/finkk.jpg'
+  import fink2 from '../../public/logos/fink2.jpg'
+  import fink3 from '../../public/logos/fink11.jpg'
+  import research from '../../public/logos/research.jpg'
+  import design from '../../public/logos/design.jpg'
+  import stats from '../../public/logos/stats.jpg'
+  import expert from '../../public/logos/uxexpert.jpg'
+  import test from '../../public/logos/fink5.jpg'
+  import statistic from '../../public/logos/statistic.jpg'
+  import { PaperClipIcon } from '@heroicons/react/24/solid'
+  
+  import { Fragment } from 'react'
+  import { Tab } from '@headlessui/react'
+  
+
+
+  
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
+  
+  
+  export default function Example() {
+    return (
+<></>
+   
+    )
+  }
+  
