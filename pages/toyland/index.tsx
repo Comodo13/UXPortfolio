e78e6fa02a446920dd69e2d8bfb2195006bbf3fa
Genmasha <marianazdar@gmail.com>
2023-11-15 17:27:00 +0100

    dima

--- pages/toyland/index.tsx
+++ pages/toyland/index.tsx
@@ -24,7 +24,7 @@ import {
   
   
   import Image from 'next/image'
-  import fink from '../../public/logos/finkk.jpg'
+  import toyland from '../../public/logos/toyland.jpg'
   import fink2 from '../../public/logos/fink2.jpg'
   import fink3 from '../../public/logos/fink11.jpg'
   import research from '../../public/logos/research.jpg'
@@ -49,9 +49,34 @@ import {
   
   export default function Example() {
     return (
-<>
-hui</>
-   
+      <div className="bg-white py-12 sm:py-24">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="text-xl font-semibold leading-7 text-blue-600">Česká Spořitelna</h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl">Clients Financial Health  </p>
+            <p className="mt-6 text-xl fint-semibold leading-6 text-gray-700">
+             Designing Financial Advisory Services for mobile banking "George",<br></br> with over 2 million daily active users.
+            </p>
+        </div>
+       </div>
+         <div className="relative overflow-hidden pt-8">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <Image
+            src={toyland}
+            alt="App screenshot"
+            className="rounded-2xl"
+            width={2432}
+            height={1442}
+          />
+          <div className="relative" aria-hidden="true">
+            <div className="absolute -inset-x-20 bottom-0 " />
+          </div>
+        </div>
+      </div>
+
+
+      </div>
+
     )
   }
   
