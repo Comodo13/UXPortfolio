67c442cf60218254b6a4f54797dc5e78acdfe101
Dima <dzmitry.rabotkin@woltair.cz>
2023-11-22 10:51:06 +0100

    ggg

--- components/Portfolio/About.tsx
+++ components/Portfolio/About.tsx
@@ -1,57 +1,5 @@
-import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import ContactOne from '../components/Contacts/ContactOne'
-import Modal from '../components/Modals/ModalOne'
-import ServiceTwo from '../components/Services/ServiceTwo'
-import product from '../resource/icons/product.png'
-import HeroTwo from '../components/Heros/HeroTwo'
-import backgroundHero from '../resource/bg/bg.png'
-import ReviewsOne from '../components/Reviews/ReviewsOne'
-import FeatureThree from '../components/Features/FeatureThree'
-import featureIconTwo from '../resource/icons/obl.jpg'
-import featureIconOne from '../resource/icons/tmin.jpg'
-import featureIconThree from '../resource/icons/boyar.jpg'
-import FeatureSeven from '../components/Features/FeatureSeven'
-import ModalInfo from '../components/Modals/ModalInfo'
-import Smartlook from 'smartlook-client'
-import FooterOne from '../components/Footers/FooterOne'
-import FeatureFive from '../components/Features/FeatureFive'
-import alarm from '../resource/icons/alarm.png'
-import heart from '../resource/icons/heart.png'
-import dna from '../resource/icons/dna.png'
-import molec from '../resource/icons/molec.png'
-import Testimonials, { CardType } from '../components/Features/FeatureFive'
-import StudyPreview from '../components/Portfolio/StudyPreview'
-import pill1 from '../resource/icons/pill1.png'
-import pill2 from '../resource/icons/pill2.png'
-import pill3 from '../resource/icons/pill3.png'
-
-import {
-  labelsTimer,
-  featureProduct,
-  titleHero,
-  textFooter,
-  titleFeature,
-  titleFeatureSeven,
-  titleReviews,
-  reviews,
-  descriptionHero,
-  buttonHero,
-  buttonFeatureSeven,
-  incentives,
-  descriptionCookie
-} from '../data/dateMX'
-import CookieOne from '../components/Banners/CookieOne'
-import About from '../components/Portfolio/About'
-
-
-
-const Home: NextPage = () => {
-
-
-  return (
-   
+export default function Example() {
+    return (
       <div className="relative bg-white">
         <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
           <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
@@ -100,11 +48,5 @@ const Home: NextPage = () => {
         </div>
       </div>
     )
-    //  <StudyPreview />
-
-     
-   
+  }
   
-}
-
-export default Home
